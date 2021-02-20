870c9986ccfd44e1d8f20e15fdde152b454a154d
Karun Jain <53487261+jainkarun8@users.noreply.github.com>
2021-02-21 00:43:29 +0530

    Add files via upload

---
+++ hw2.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<head>
+<title>
+19BIT0350 Course credits</title></head>
+<body>
+<?php
+$OSP = 4;
+$ALA = 4;
+$CAO = 3;
+$OOAD = 4;
+$LeanStartupManagment = 2;
+$TheoryOfComputation = 3;
+$Apti = 2;
+$OS = 4;
+$Total = $ALA+$Apti+$CAO+$OSP+$OS+$TheoryOfComputation+$OOAD+$LeanStartupManagment;
+echo "Total Credits : $Total";
+?>
+</body>
+</head>
